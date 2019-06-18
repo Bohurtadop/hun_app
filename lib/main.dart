a5bc6805dd84342c23830f29ebd34a7a374e8f5c
HASPIMA <hpintom@unal.edu.co>
2019-06-18 17:17:01 -0500

    App is now connected to firebase. ·Auth provider was added which is an inherited widget. It uses Auth class. ·Auth class was created, this one allows the app the connection with firebase auth. ·Root page was created. This widget manages the app's state considering the user's auth state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:hun_app/Animations/Loading.dart';
+import 'package:hun_app/auth/auth.dart';
+import 'package:hun_app/auth/auth_provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    debugShowMaterialGrid: false,
-    checkerboardOffscreenLayers: false,
-    checkerboardRasterCacheImages: false,
-    theme: ThemeData(fontFamily: 'Ancízar Sans Regular',
-      primaryColor: Color(0xff1266A4),
-      accentColor: Color(0xff1266A4),
-      dialogBackgroundColor: Color.fromRGBO(255, 255, 255, 0.95),
-    ),
-    home: Loading(),
-  ));
+  runApp(new HUN());
+}
+
+class HUN extends StatelessWidget {
+  const HUN({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        debugShowMaterialGrid: false,
+        checkerboardOffscreenLayers: false,
+        checkerboardRasterCacheImages: false,
+        theme: ThemeData(
+          fontFamily: 'Ancízar Sans Regular',
+          primaryColor: Color(0xff1266A4),
+          accentColor: Color(0xff1266A4),
+          dialogBackgroundColor: Color.fromRGBO(255, 255, 255, 0.95),
+        ),
+        home: Loading(),
+      ),
+    );
+  }
 }
