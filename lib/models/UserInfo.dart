df4a05bf56fcc999c37edc0f0afa62ff0bf0a632
HASPIMA <hpintom@unal.edu.co>
2019-08-07 18:40:08 -0500

    UserInfo class now returns a widget StreamBuilder. · If an error happens will return a Text widget. · If it is waiting for the information will return a Text widget. · If the document has data it will display the user's id.

--- lib/models/UserInfo.dart
+++ lib/models/UserInfo.dart
@@ -1,15 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 
-class UserInfo extends StatelessWidget{
+class UserInfo extends StatelessWidget {
   const UserInfo({this.uid});
   final String uid;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return StreamBuilder<DocumentSnapshot>(
+      stream:
+          Firestore.instance.collection('users').document(this.uid).snapshots(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return new Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+          //TODO: Loading widget
+            return new Text('Loading...');
+            break;
+          default:
+            if (snapshot.hasData) {
+              DocumentSnapshot userDocument = snapshot.data;
+              //TODO: Show user's name and type of user
+              return Text(userDocument['uid']);
+            } else {
+              return Text('Document has no info');
+            }
+        }
+      },
+    );
   }
-
-  
-  
 }
