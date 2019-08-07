11690ac33e3539cfb739b05138ea9297330e41d4
HASPIMA <hpintom@unal.edu.co>
2019-08-07 18:19:02 -0500

    User Info class was created.

---
+++ lib/models/UserInfo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class UserInfo extends StatelessWidget{
+  const UserInfo({this.uid});
+  final String uid;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+  
+  
+}
