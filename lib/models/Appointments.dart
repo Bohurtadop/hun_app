d8165fb200be9b01896212bae9ba5b86c1622fb1
HASPIMA <hpintom@unal.edu.co>
2019-08-08 11:26:22 -0500

    Appointments class was created. If structure in UserInfo was modified, it does the same.

---
+++ lib/models/Appointments.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Appointments extends StatelessWidget {
+  const Appointments({this.uid});
+  final String uid;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
