2716e4386ff41ab78ac7bd3bbc3ced155bfb274b
HASPIMA <hpintom@unal.edu.co>
2019-08-08 11:40:49 -0500

    Appointments will get all the documents where the client is the actual user. Cloud firestore was updated because it is needed to use a collection group query.

--- lib/models/Appointments.dart
+++ lib/models/Appointments.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,8 +8,14 @@ class Appointments extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance
+          .collectionGroup('appointments')
+          .where('uid', isEqualTo: this.uid)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        return null;
+      },
     );
   }
 }
