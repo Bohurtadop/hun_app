d4162c662f5385df503c635cd6fbdb5f907ae6c7
HASPIMA <hpintom@unal.edu.co>
2019-08-19 15:52:04 -0500

    Unavailable messages in Profile don't move the floating button.

--- lib/Screens/Profile.dart
+++ lib/Screens/Profile.dart
@@ -8,7 +8,9 @@ import 'package:hun_app/resources/Resources.dart';
 
 class Profile extends StatefulWidget {
   final String uid;
+
   Profile({@required this.uid});
+
   @override
   createState() => ProfileState();
 }
@@ -60,44 +62,50 @@ class ProfileState extends State<Profile> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(255, 255, 255, 0.95),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.9,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              spaceBetween(40),
-              hunLogoAndTittle(context),
-              _profilePhoto(),
-              UserInfo(uid: widget.uid),
-              spaceBetween(10),
-              mainButton(
-                context: context,
-                buttonText: "Editar datos",
-                height: MediaQuery.of(context).size.height / 16,
-                width: MediaQuery.of(context).size.width / 2,
+      body: Builder(
+        builder: (BuildContext context) {
+          return Scaffold(
+            backgroundColor: Color.fromRGBO(255, 255, 255, 0.95),
+            body: SingleChildScrollView(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.9,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    spaceBetween(40),
+                    hunLogoAndTittle(context),
+                    _profilePhoto(),
+                    UserInfo(uid: widget.uid),
+                    spaceBetween(10),
+                    mainButton(
+                      context: context,
+                      buttonText: "Editar datos",
+                      height: MediaQuery.of(context).size.height / 16,
+                      width: MediaQuery.of(context).size.width / 2,
+                    ),
+                    spaceBetween(5),
+                    mainButton(
+                      context: context,
+                      buttonText: "Mi historia clínica",
+                      height: MediaQuery.of(context).size.height / 16,
+                      width: MediaQuery.of(context).size.width / 2,
+                    ),
+                    spaceBetween(10),
+                    offTopicButton(
+                      context: context,
+                      buttonText: "Cerrar sesión",
+                      height: MediaQuery.of(context).size.height / 16,
+                      width: MediaQuery.of(context).size.width / 2.1,
+                      onPressed: this._signOut,
+                    ),
+                    spaceBetween(20),
+                  ],
+                ),
               ),
-              spaceBetween(5),
-              mainButton(
-                context: context,
-                buttonText: "Mi historia clínica",
-                height: MediaQuery.of(context).size.height / 16,
-                width: MediaQuery.of(context).size.width / 2,
-              ),
-              spaceBetween(10),
-              offTopicButton(
-                context: context,
-                buttonText: "Cerrar sesión",
-                height: MediaQuery.of(context).size.height / 16,
-                width: MediaQuery.of(context).size.width / 2.1,
-                onPressed: this._signOut,
-              ),
-              spaceBetween(20),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
